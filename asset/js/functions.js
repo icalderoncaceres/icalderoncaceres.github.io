813a204052b5afb50406ce01fc98811daf2011d8
ivan calderon <ivandario2010@gmail.com>
2018-03-20 22:43:19 -0500

    Se ajusto la maquetación del index y se creo la página de projects.htmls

--- asset/js/functions.js
+++ asset/js/functions.js
@@ -1,5 +1,37 @@
 var lang="en";
-import lang_obj from '../lang/lang.js';
+//import lang_obj from '../lang/lang.js';
+var lang_obj={
+	home:{es:"Inicio",en:"Home"},
+	styles:{es:"Estilos",en:"Styles"},
+	cv:{es:"Curriculum",en:"CV"},
+	projects:{es:"Proyectos",en:"Projects"},
+	codes:{es:"Códigos",en:"Codes"},
+	news:{es:"Noticias",en:"News"},
+	firstLanguage:{es:"Español",en:"English"},
+	secondLanguage:{es:"Ingles",en:"Spanish"},
+	welcome:{es:" les da la bienvenida",en:" gives you welcome"},
+	thanks:{
+		es:"Muchas gracias por visitar esté sitio, el cual tiene la finalidad de compartir noticias, códigos e información en general relacionado con el desarrollo de software",
+		en:"Thank you very much for visiting this site, which has the purpose of sharing news, codes and information in general related to software development",
+	},
+	socialNetworks:{es:'Por favor visita mis redes sociales',en:'Please visit my social networks'},
+	contactus:{es:'Contactanos',en:'Contact us'},
+	name:{es:'Nombre',en:'Name'},
+	nameValidation:{es:'Este nombre de no es válido',en:'This name is not valid'},
+	emailValidation:{es:'Este email no es válido',en:'This email is not valid'},
+	email:{es:'Correo electónico',en:'Email'},
+	subject:{es:'Asunto',en:'Subject'},
+	select:{es:'Seleccione',en:'Select'},
+	question:{es:'Pregunta',en:'Question'},
+	suggestion:{es:'Sugerencia',en:'Suggestion'},
+	otherSubject:{es:'Otro asunto',en:'Other subject'},
+	message:{es:'Mensaje',en:'Message'},
+	send:{es:'Enviar',en:'Send'},
+	cancel:{es:'Cancelar',en:'Cancel'},
+	repositories:{es:'Repositorios',en:'repositories'},
+	all:{es:'Todos',en:'All'}
+}
+
 $(document).ready(function(){
 	setLanguage(lang);
 
